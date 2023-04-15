9fb27d2ca330497d6d92903e1e460e31f68116b7
Milton Junior <milton.junior@sillion.com.br>
2023-04-15 18:18:54 -0300

    Using until in ruby

--- first_project.rb
+++ first_project.rb
@@ -39,8 +39,17 @@ class Carro
         end
         
     end
+
+    def contadorUntil()
+        i = 0
+        until i == 5 do
+            puts i
+            i+=1
+        end
+    end
 end
 
 carro = Carro.new "Ford","Model 2"
 puts carro.verifyOwner
 puts carro.contador
+puts carro.contadorUntil
